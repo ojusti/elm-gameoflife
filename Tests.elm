986399f6cea65f03db40d60e1c30f2964c8a96de
Justinian Oprescu <ojusti@yahoo.fr>
2015-11-28 23:00:53 +0200

    Add new test

--- Tests.elm
+++ Tests.elm
@@ -9,7 +9,12 @@ all =
     [
       test "make oscilate 3 cells" (
         assertEqual
-          ([[0,0],[1,0],[2,0]] |> GameOfLife.tick |> GameOfLife.tick)
           [[0,0],[1,0],[2,0]]
+          ([[0,0],[1,0],[2,0]] |> GameOfLife.tick |> GameOfLife.tick)
+      ),
+      test "order cells in universe by coordinates" (
+        assertEqual
+          [[0,0],[0,1],[1,-1],[1,0],[1,1],[2,0],[2,1]]
+          (GameOfLife.tick [[0,0],[1,0],[2,0],[1,1]])
       )
     ]
